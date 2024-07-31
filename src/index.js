cd65d086334f2a4f98d3bcd5b7dac58094de9265
FullofJuice <lehajuiced1996@gmail.com>
2024-07-31 14:17:11 +0300

    feat: complete reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = -n;
+    }
+
+    let str = n + '';
+    let reversedString = '';
+
+    for (let i = 0; i < str.length; i++) {
+        reversedString = str[i] + reversedString;
+    }
+
+    return +reversedString;
+
 }
